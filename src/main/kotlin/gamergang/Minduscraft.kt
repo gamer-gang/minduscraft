aad00de685dbabdefb7cbc698b8521e61e3f317f
wiisportsresort <devdoge1@gmail.com>
2021-04-08 13:52:51 -0400

    add stuff

---
+++ src/main/kotlin/gamergang/Minduscraft.kt
@@ -0,0 +1,10 @@
+package gamergang
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+class Minduscraft : ModInitializer {
+    override fun onInitialize() {
+        println("inside Minduscraft#onIntialize")
+    }
+}
